c45a648aab9e01966088dc8d2c73e20ba1fcfcda
Sage Thomas <sage.thomas@outlook.com>
2017-09-18 12:32:00 -0600

    added model methods

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,3 +1,25 @@
 class Alert < ApplicationRecord
   has_many :deals
+
+  # determines if a deal has been found and goes from there
+  def new_deal_routine
+    return if self.state == 0
+
+    deal = Deal.order(:price).first
+    if deal.price <= self.price_limit || self.auto_alert?
+      
+      self.update(state: 0)
+      # delete deal
+    end
+  end
+
+  # returns true if the alert should be activated
+  def auto_alert?
+    return false
+  end
+
+  def email_best_deal
+
+  end
+
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,3 +1,13 @@
 class Deal < ApplicationRecord
   belongs_to :alerts, optional: true
+
+  # Destroy all deals without an alert_id and over a day old
+  def self.destroy_old_and_orphaned
+    Deal.delete_all(["alert_id IS NULL AND updated_at < ?", 1.days.ago])
+  end
+
+  def self.destroy_old
+    Deal.delete_all(["updated_at < ?", 1.days.ago])
+  end
+
 end
