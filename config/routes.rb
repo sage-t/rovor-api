b7949b622ce5f45469c2b86013ad7da7a410d78e
Sage Thomas <sage.thomas@outlook.com>
2017-09-18 12:29:34 -0600

    added queue and metadata routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :deals
   resources :alerts
+
+  get '/queue/', to: 'alerts#queue'
+  get '/metadata/', to: 'alerts#metadata'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
