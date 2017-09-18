6d66b47bb9ef52c771ca9f8ba6582203cbc530a8
Sage Thomas <sage.thomas@outlook.com>
2017-09-18 12:28:02 -0600

    added queue and metadata routes

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,9 +1,34 @@
 class AlertsController < ApplicationController
   before_action :set_alert, only: [:show, :update, :destroy]
 
+  # returns the first n alerts that haven't been searched in the longest time
+  # GET /alerts/queue/?limit
+  def queue
+    limit = params[:limit] ? params[:limit].to_i : 1 
+
+    @alerts = Alert.where('0 < state').order('last_searched NULLS FIRST').limit(limit)
+
+    render json: @alerts
+  end
+
+  # Returns the amount of alerts
+  # GET /metadata
+  def metadata
+    metadata = Hash.new
+    metadata[:alert_count] = Alert.count
+    metadata[:active_alert_count] = Alert.where('0 < state').count
+    metadata[:deal_count] = Deal.count
+
+    render json: metadata
+  end
+
   # GET /alerts
   def index
-    @alerts = Alert.all
+    if params[:user_id]
+      @alerts = Alert.where user_id: params[:user_id]
+    else
+      @alerts = Alert.all
+    end
 
     render json: @alerts
   end
